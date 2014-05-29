34aafcd7a35f3d66aec35ee9af8763ba35652868
Quin <qjshanley@gmail.com>
2014-05-29 09:58:46 -0500

    initial add of mongorc.js file

---
+++ mongorc.js
@@ -0,0 +1,16 @@
+function objectIdFromTimestamp(ts) {
+        // ***** USE 'YYYY/MM/DD' FORMAT *****
+
+        // Convert string date to Date object (otherwise assume timestamp is a date)
+        if (typeof(ts) == 'string') {
+                var ts = new Date(ts);
+        }
+
+        // Convert date object to hex seconds since Unix epoch
+        var hexSeconds = Math.floor(ts/1000).toString(16);
+
+        // Create an ObjectId with that hex timestamp
+        var constructedObjectId = ObjectId(hexSeconds + "0000000000000000");
+
+        return constructedObjectId
+}
