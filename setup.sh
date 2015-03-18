e38aee3a236fe67bfdbff759dad82f359044c149
Quinlan Shanley <qjshanley@gmail.com>
2015-03-18 16:27:18 -0500

    I don't track the .profile anymore.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 ln -sft ~/. ~/.environment_configs/.bash_aliases
 ln -sft ~/. ~/.environment_configs/.mongorc.js
-ln -sft ~/. ~/.environment_configs/.profile
 ln -sft ~/. ~/.environment_configs/.screenrc
 ln -sft ~/. ~/.environment_configs/.vimrc
 ln -sft ~/.ssh/. ~/.environment_configs/config
