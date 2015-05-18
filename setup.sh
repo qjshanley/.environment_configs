f2a172ee32f82fcf929fac00e84acfb9fea90329
Quinlan Shanley <qjshanley@gmail.com>
2015-05-18 11:35:13 -0500

    I don't like managing the config. Names change too frequently.

--- setup.sh
+++ setup.sh
@@ -2,8 +2,6 @@ ln -sft ~/. ~/.environment_configs/.bash_aliases
 ln -sft ~/. ~/.environment_configs/.mongorc.js
 ln -sft ~/. ~/.environment_configs/.screenrc
 ln -sft ~/. ~/.environment_configs/.vimrc
-ln -sft ~/.ssh/. ~/.environment_configs/config
-chmod 644 ~/.environment_configs/config
 
 if ! grep -Fxq "if [ -f ~/.bash_aliases ]; then" ~/.bashrc
 then
