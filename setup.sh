0815fd2a2f6cdf6dc77d93fd4db795cc960aafe3
Highly <highly@Quins-MacBook-Pro.local>
2016-12-21 13:00:53 -0600

    configs go in their own folder.

--- setup.sh
+++
@@ -1,9 +0,0 @@
-ln -sft ~/. ~/.environment_configs/.bash_aliases
-ln -sft ~/. ~/.environment_configs/.mongorc.js
-ln -sft ~/. ~/.environment_configs/.screenrc
-ln -sft ~/. ~/.environment_configs/.vimrc
-
-if ! grep -Fxq "if [ -f ~/.bash_aliases ]; then" ~/.bashrc
-then
-    printf "# User specific aliases and functions\nif [ -f ~/.bash_aliases ]; then\n\t. ~/.bash_aliases\nfi\n" >>~/.bashrc
-fi
