b0d9ef776b19d739e03880ad85ef8345e3d6b42d
Quinlan Carlos-Shanley <quin.shanley@gmail.com>
2018-03-17 19:04:02 -0500

    better datica autocomplete

--- .bin/autocompletion_scripts/datica_autocomplete.sh
+++ .bin/autocompletion_scripts/datica_autocomplete.sh
@@ -1,17 +1,17 @@
 #! /usr/bin/env bash
 
 _commands() {
-  eval "$@ --help" 2>&1 | awk '/^Commands:/ {start=1; next} start==1 && !/^$/ {print $1} $1 ~ /^$/ {start=0}'
+  datica $@ --help 2>&1 | awk '/^Commands:/ {start=1; next} start==1 && !/^$/ {print $1} $1 ~ /^$/ {start=0}'
 }
 
 _datica_autocomplete() {
   local cur prev opts
   COMPREPLY=()
   cur="${COMP_WORDS[COMP_CWORD]}"
-  prev="${COMP_WORDS[COMP_CWORD-1]}"
-  opts="$(_commands "${COMP_WORDS[*]}")"
+  prev="${COMP_WORDS[*]:1:$COMP_CWORD-1}"
+  opts="$(_commands "$prev")"
   COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
   return 0
 }
 
- complete -F _datica_autocomplete datica
+complete -F _datica_autocomplete datica
