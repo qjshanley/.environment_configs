79376043c0c880e60800527461dc2a1f79308869
Quinlan Carlos-Shanley <quin.shanley@gmail.com>
2018-05-07 06:38:49 -0500

    tested and working

--- install_gnu_software.sh
+++ install_gnu_software.sh
@@ -2,31 +2,39 @@
 
 DEFAULT_URL_PATTERN='http://ftpmirror.gnu.org/${SOFTWARE}/${SOFTWARE}-${VERSION}.${EXTENSION}'
 declare -A GNU_SOFTWARE_LIST=(
-  [0,"SOFTWARE"]="sed"
-  [0,"VERSION"]="4.5"
-  [0,"EXTENSION"]="tar.xz"
+  [0,"SOFTWARE"]="bash"
+  [0,"VERSION"]="4.4.18"
+  [0,"EXTENSION"]="tar.gz"
   [0,"URL"]="$DEFAULT_URL_PATTERN"
 
   [1,"SOFTWARE"]="screen"
   [1,"VERSION"]="4.6.2"
   [1,"EXTENSION"]="tar.gz"
   [1,"URL"]="$DEFAULT_URL_PATTERN"
+
+  [2,"SOFTWARE"]="gawk"
+  [2,"VERSION"]="4.2.1"
+  [2,"EXTENSION"]="tar.xz"
+  [2,"URL"]="$DEFAULT_URL_PATTERN"
+
+  [3,"SOFTWARE"]="sed"
+  [3,"VERSION"]="4.5"
+  [3,"EXTENSION"]="tar.xz"
+  [3,"URL"]="$DEFAULT_URL_PATTERN"
 )
 
 i=0
 while [ -n "${GNU_SOFTWARE_LIST["$i","SOFTWARE"]}" ] ; do
-  mkdir -p /tmp/gnu && cd /tmp/gnu
-
   SOFTWARE="${GNU_SOFTWARE_LIST[$i,SOFTWARE]}"
   VERSION="${GNU_SOFTWARE_LIST[$i,VERSION]}"
   EXTENSION="${GNU_SOFTWARE_LIST[$i,EXTENSION]}"
   URL="$(eval printf "%s" "${GNU_SOFTWARE_LIST[$i,URL]}")"
 
+  printf "%s\n" "mkdir -p /tmp/gnu && cd /tmp/gnu"
   printf "%s\n" "curl -JOL $URL"
   printf "%s\n" "tar xzvf ${SOFTWARE}-${VERSION}.${EXTENSION}"
   printf "%s\n" "cd ${SOFTWARE}-${VERSION}"
   printf "%s\n" "./configure --prefix=/usr/local && make && sudo make install"
-  printf "done\n\n"
 
   i=$(( $i + 1 ))
 done
