3b40803dd538b22516578ab2c3f21a04932ce927
Quinlan Shanley <quin.shanley@gmail.com>
2018-07-06 22:15:52 -0500

    Auto stash before merge of "master" and "origin/master"

--- setup.sh
+++ setup.sh
@@ -7,13 +7,9 @@ set -e
 ENV_DIR=~/code/qub3r/environment
 
 # ln files to the correct places
-if [ -d "${ENV_DIR}/configs" ]; then
-
-  # dot files
-  for config in $(ls -A "${ENV_DIR}/configs/."); do
-    ln -sf "${ENV_DIR}/configs/${config}" ~/.
-  done
-
-  # bash autocomplete
-  ln -sf "${ENV_DIR}/.bash_autocomplete" ~/.
+if [ -d ${ENV_DIR}/configs ]; then
+	for object in $(ls -A ${ENV_DIR}/configs) ; do
+		rm -rf ~/$object
+		ln -s ${ENV_DIR}/configs/${object} ~/${object}
+	done
 fi
