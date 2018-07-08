5e1601d7bb491797d7175247b3ae8ddfe080400c
Quinlan Carlos-Shanley <quin.shanley@gmail.com>
2018-07-08 14:08:59 -0500

    cleaner

--- files/.bin/datica.ssh.sh
+++ files/.bin/datica.ssh.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-HOST=$1
 DOCKER_CMD=( $(pbpaste) )
 JOBID=${DOCKER_CMD[4]}
 
 IFS='' read -r -d '' SCRIPT <<- EOF
-	if [ -z "\$(grep "^alias dexec_${JOBID}")" ] ; then
-		printf -- '%s\n' 'alias dexec_${JOBID}="sudo docker exec -it -e TERM=xterm -e EDITOR=vim -e VISUAL=vim $JOBID bash -o vi"' >> ~/.bash_temporary
+	if [ -z "\$(grep "^alias dexec_${JOBID}" ~/.bash_temporary)" ] ; then
+	  printf -- '%s\n' 'alias dexec_${JOBID}="sudo docker exec -it -e TERM=xterm -e EDITOR=vim -e VISUAL=vim $JOBID bash -o vi"' >> ~/.bash_temporary
 	fi
 
 	screen -S ssh -X screen -t $JOBID
@@ -17,18 +16,16 @@ IFS='' read -r -d '' SCRIPT <<- EOF
 	screen -U -DR -S ssh -p $JOBID
 EOF
 
-printf -v SSH_SCREEN -- '%q' "$SCRIPT"
-
 # copy dot files to server
 rsync -Le ssh \
 	~/.bash_profile \
 	~/.bash_aliases \
-	~/.bash_logout
+	~/.bash_logout \
 	~/.bashrc \
 	~/.screenrc \
 	~/.vimrc \
-	"$HOST":~/. >/dev/null 2>&1
+	"$1":~/. >/dev/null 2>&1
 
 # ssh to server and attach directly to a screen session
-ssh -t "$HOST" "$SCRIPT"
+ssh -t "$1" "$SCRIPT"
 
