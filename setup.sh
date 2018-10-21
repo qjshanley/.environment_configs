395f65cc2784379977679301f61d4609a064f6a9
Quinlan Carlos-Shanley <quin.shanley@gmail.com>
2018-10-20 20:36:46 -0500

    checkin

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ ENV_FILES="${1:?Please provide the base dir of the repo}"/files
 if [ -d "$ENV_FILES" ]; then
 
 	# make sure directories exist
-        ( cd "$ENV_FILES" ; find . -type d -mindepth 1 -print0 | tar czh --null --files-from /dev/stdin --no-recursion ) | ( cd ; tar x )
+        ( cd "$ENV_FILES" ; find . -type d -mindepth 1 -print0 | tar czh --null --files-from /dev/stdin ) | ( cd ; tar xz )
 
 	# symlink files to the correct directories
 	for target in $(cd "$ENV_FILES" ; find . -mindepth 1 -type f -or -type l -and -not -name "*swp") ; do
