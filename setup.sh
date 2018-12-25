a91d1dc7fe2945c060c640f8566cb7761473ac10
Quinlan Shanley <quin.shanley@gmail.com>
2018-12-25 15:35:21 -0600

    update

--- setup.sh
+++ setup.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
-IFS=$'\t\n'
 
-# environment files
-ENV_FILES="${1:?Please provide the base dir of the repo}"/files
+BASE="$(dirname "$0")"
+ENV_FILES="${BASE}/files"
 
 if [ -d "$ENV_FILES" ]; then
+    # make sure directories exist
+    ( cd "$ENV_FILES" ; find . -type d -mindepth 1 -print0 | tar czh --null --files-from /dev/stdin ) | ( cd ; tar xz )
 
-	# make sure directories exist
-        ( cd "$ENV_FILES" ; find . -type d -mindepth 1 -print0 | tar czh --null --files-from /dev/stdin ) | ( cd ; tar xz )
-
-	# symlink files to the correct directories
-	for target in $(cd "$ENV_FILES" ; find . -mindepth 1 -type f -or -type l -and -not -name "*swp") ; do
-		#link_name=$(sed -E "s@${ENV_FILES}/(.*)@\1@" <<<"$target")
-		ln -fsrL "$ENV_FILES"/"$target" ~/"$target"
-	done
+    # symlink files to the correct directories
+    for target in $(cd "$ENV_FILES" ; find . -mindepth 1 -type f -or -type l -and -not -name "*swp") ; do
+        ln -fsrL "$ENV_FILES"/"$target" ~/"$target"
+    done
+else
+    echo "Could not find environment files"
 fi
