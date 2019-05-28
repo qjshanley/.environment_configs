a0e45ccfbdac69efd3174acc80ddf5b95782c8c5
Quinlan Carlos-Shanley <quin.shanley@gmail.com>
2019-05-28 10:54:10 -0500

    added elastic helper

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ ENV_FILES="${BASE}/files"
 
 if [ -d "$ENV_FILES" ]; then
     # make sure directories exist
-    ( cd "$ENV_FILES" ; find . -type d -mindepth 1 -print0 | tar czh --null --files-from /dev/stdin ) | ( cd ; tar xz )
+    ( cd "$ENV_FILES" ; find . -mindepth 1 -type d -print0 | tar czh --null --files-from /dev/stdin ) | ( cd ; tar xz )
 
     # symlink files to the correct directories
     for target in $(cd "$ENV_FILES" ; find . -mindepth 1 -type f -or -type l -and -not -name "*swp") ; do
