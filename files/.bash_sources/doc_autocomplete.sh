d91370ec247f62d52d4fcea8d7e1482d4a59bbd2
Quinlan <quin.shanley@gmail.com>
2020-10-18 09:25:05 -0500

    doc updates complete

--- files/.bash_sources/doc_autocomplete.sh
+++ files/.bash_sources/doc_autocomplete.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
 _doc_opts() {
-    local stack="$(doc utils -C "$1")"
+    local stack="$(doc utils --check-stack "$1")"
     [ "$stack" == "$1" ] && shift 1
     case "$1" in
         '') 
-            if [ ! -e /tmp/doc/commands ] ; then
+            if [ ! -e "$(find /tmp/doc/ -name commands -mtime 0)" ] ; then
                 mkdir -p /tmp/doc
                 {
                     docker-compose  --help | awk '/^Commands:/,/$1/ { if ($1 ~ /^[a-z]*$/) print $1 }'
                     awk '$3 ~ /^_doc_/ { split($0, cmd, "_") ; print cmd[3] }' $(which doc)
                 } | sort | uniq > /tmp/doc/commands
+                echo utils >> /tmp/doc/commands
             fi
             cat /tmp/doc/commands
             ;;
@@ -42,7 +43,7 @@ _doc_autocomplete() {
         # the STACKS variable exists and we can proceed.
         if [ "${#prev[@]}" == "0" ] ; then
             # no previous words found. we are completing a path. return stack options.
-            local opts="$(doc utils -stacks | cut -d '/' -f1 | sort | uniq)"
+            local opts="$(doc utils --stacks)"
         else
             # previous words found. we are completing docker arguments.
             local opts="$(_doc_opts "${prev[@]}")"
